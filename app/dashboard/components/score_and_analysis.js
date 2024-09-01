e909af50d0fedd553942983a91085bf6f2fb0784
Luis Arellano <arellano.luis88@gmail.com>
2024-08-31 22:25:40 -0700

    added icons to the keywords

--- app/dashboard/components/score_and_analysis.js
+++ app/dashboard/components/score_and_analysis.js
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 import { ModelContext } from '../context';
 import apiClient from '@/libs/api';
 
@@ -7,6 +8,8 @@ const ScoreAnalysis = () => {
 
     const {selectedModel, latestJobScan, setLatestJobScan, jobScanRefreshKey } = useContext(ModelContext);
     const [isLoading, setIsLoading] = useState(false);
+    const [matchedKeywords, setMatchedKeywords] = useState([]);
+
 
     useEffect(() => {
         const fetchJobScan = async () => {
@@ -27,6 +30,18 @@ const ScoreAnalysis = () => {
         fetchJobScan();
     }, [selectedModel, jobScanRefreshKey]);
 
+    useEffect(() => {
+        if (latestJobScan && selectedModel) {
+            console.log('latestJobScan:', selectedModel);
+            const resumeText = JSON.stringify(selectedModel.resume_data).toLowerCase();
+            const keywords = latestJobScan.keywords ? latestJobScan.keywords.split(', ') : [];
+            const matched = keywords.filter(keyword => resumeText?.includes(keyword.toLowerCase()));
+            setMatchedKeywords(matched);
+        } else {
+            setMatchedKeywords([]);
+        }
+    }, [latestJobScan, selectedModel]);
+
     if (isLoading) {
         return <div>Loading job scan...</div>;
     }
@@ -37,7 +52,6 @@ const ScoreAnalysis = () => {
 
     const keywords = latestJobScan.keywords ? latestJobScan.keywords.split(', ') : [];
 
-
     return (
         <div className="w-full p-4 mx-auto bg-white border border-1 border-grey rounded-lg shadow-md">
             <h2 className="text-xl font-bold mb-2">Resume Analysis </h2>
@@ -45,7 +59,19 @@ const ScoreAnalysis = () => {
             <div className='Scores text-xs'>
                 <h3 className="text-lg font-semibold">Scores</h3>
                 <br/>
-                <p>{keywords.length > 0 ? keywords.join(', ') : 'No keywords available'}</p>
+                {/* <p>{keywords.length > 0 ? keywords.join(', ') : 'No keywords available'}</p> */}
+                <div className="flex flex-wrap gap-2">
+                    {latestJobScan.keywords ? latestJobScan.keywords.split(', ').map((keyword, index) => (
+                        <div key={index} className="flex items-center">
+                            {matchedKeywords.includes(keyword) ? (
+                                <FaCheckCircle className="text-green-500 mr-1" />
+                            ) : (
+                                <FaTimesCircle className="text-red-500 mr-1" />
+                            )}
+                            <span>{keyword}</span>
+                        </div>
+                    )) : 'No keywords available'}
+                </div>
             </div>
 
             <hr className="my-4" />
