60b9a3d730456fc27689a5224a23b9be50b7e7c0
Luis Arellano <arellano.luis88@gmail.com>
2024-09-16 22:43:55 -0700

    increase text area

--- app/dashboard/components/job_scan.js
+++ app/dashboard/components/job_scan.js
@@ -239,6 +239,7 @@ function JobScan() {
               value={jobDescription}
               onChange={handleJobDescriptionChange}
               placeholder="Paste the job description here..."
+              rows={10}
               className="mt-1 block w-full 
               px-3 py-2 border border-gray-300 
               rounded-md shadow-sm 
